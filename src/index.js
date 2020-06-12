bc0ec4b310ce06df6122e7649ddcaa38c6542a4f
OlushkaZ <gincgo@yandex.ru>
2020-06-12 10:27:53 +0300

    main

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.jsx";
+
+
+const init = () => {
+  const settings = {
+    offerCount: 312,
+  };
+  ReactDOM.render(<App offerCount={settings.offerCount}/>, document.querySelector(`#root`));
+};
+
+init();
